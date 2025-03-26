bcee366de71dc0c82425a5506435e5bf02e79faf
Adrian Kahiga <kahigaadrian7@gmail.com>
2025-03-26 13:00:19 +0300

    fixed npm issue

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(pickupLocation) {
+function distanceFromHqInBlocks(someValue) {
     const hqLocation = 42; 
-    return Math.abs(pickupLocation - hqLocation); 
-  }
-  function distanceFromHqInFeet(pickupLocation) {
-    const blocks = distanceFromHqInBlocks(pickupLocation); 
-    return blocks * 264; 
-  }
-  function distanceTravelledInFeet(startBlock, endBlock) {
-    return Math.abs(endBlock - startBlock) * 264; 
+    return Math.abs(someValue - hqLocation);
   }
   
-  function calculatesFarePrice(start, destination) {
-  const distance = distanceTravelledInFeet(start, destination); 
-
-  if (distance <= 400) {
-    return 0; 
-  } else if (distance > 400 && distance <= 2000) {
-    return (distance - 400) * 0.02; 
-  } else if (distance > 2000 && distance <= 2500) {
-    return 25; 
-  } else {
-    return 'cannot travel that far'; 
+  
+  function distanceFromHqInFeet(someValue) {
+    const feetPerBlock = 264; 
+    return distanceFromHqInBlocks(someValue) * feetPerBlock;
   }
+  
+  
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    return Math.abs(destination - start) * feetPerBlock;
+  }
+  
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= 400) {
+      return 0; 
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02; 
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25; 
+    } else {
+      return 'cannot travel that far'; 
     }
+  }
